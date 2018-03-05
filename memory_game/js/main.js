ce9b3901d0026de41de0d013c28283c7a61ce32b
Chuong Dao <chuongdao@Chuongs-MacBook-Pro.local>
2018-03-05 14:38:38 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,61 @@
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert('You found a match!');
+		} else {
+			alert('Sorry, try again.');
+		}
+	}
+}
+
+var flipCard = function(cardId) {
+	var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+
+	console.log('User flipped ' + cards[cardId].rank)
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+}
+flipCard(0);
+flipCard(2);
+checkForMatch()
+
+
+
+
+
+
+
+
+
+
+
+
